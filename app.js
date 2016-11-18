79293d5670403954fa5fe7eb3db940d326158b26
Koki Takahashi <hakatasiloving@gmail.com>
2016-11-18 23:05:10 +0900

    Serve configs from .env file

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const sass = require('node-sass-middleware');
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-dotenv.load({ path: '.env.example' });
+dotenv.load({ path: '.env' });
 
 /**
  * Controllers (route handlers).
