6dde8c4f28dafa55725150e1dd1434f90e13e13f
Koki Takahashi <hakatasiloving@gmail.com>
2016-11-18 23:18:24 +0900

    Fix ESLint issues

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,6 @@
 const async = require('async');
 const crypto = require('crypto');
 const nodemailer = require('nodemailer');
-const passport = require('passport');
 const User = require('../models/User');
 
 /**
