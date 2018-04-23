adcc2061a176fff745f1372dbfd0aadd6d35fa21
Koki Takahashi <hakatasiloving@gmail.com>
2018-04-24 01:29:43 +0900

    Add languages and secrets of old contests

---
+++ languages.v1.js
@@ -0,0 +1,350 @@
+module.exports = [
+  // Line 1
+  {
+    type: 'language',
+    slug: 'cubix',
+    name: 'Cubix',
+  }, {
+    type: 'language',
+    slug: 'matl',
+    name: 'MATL',
+  }, {
+    type: 'language',
+    slug: 'element',
+    name: 'Element',
+  }, {
+    type: 'language',
+    slug: 'hbcht',
+    name: 'Half-Broken Car in Heavy Traffic',
+  }, {
+    type: 'language',
+    slug: 'goto10',
+    name: 'GOTO 10',
+  }, {
+    type: 'language',
+    slug: 'aheui',
+    name: 'Aheui',
+  }, {
+    type: 'language',
+    slug: 'zucchini',
+    name: 'Zucchini',
+  }, {
+    type: 'language',
+    slug: 'ubergenes',
+    name: 'UberGenes',
+  }, {
+    type: 'language',
+    slug: '3var',
+    name: '3var',
+  },
+
+  // Line 2
+  {
+    type: 'language',
+    slug: 'japt',
+    name: 'Japt',
+  }, {
+    type: 'language',
+    slug: 'evil',
+    name: 'Evil',
+  }, {
+    type: 'language',
+    slug: 'cjam',
+    name: 'CJam',
+  }, {
+    type: 'language',
+    slug: 'emmental',
+    name: 'Emmental',
+  }, {
+    type: 'language',
+    slug: 'goruby',
+    name: 'Goruby',
+  }, {
+    type: 'language',
+    slug: 'unreadable',
+    name: 'Unreadable',
+  }, {
+    type: 'language',
+    slug: 'fish',
+    name: '><>',
+  }, {
+    type: 'language',
+    slug: 'glass',
+    name: 'Glass',
+  }, {
+    type: 'language',
+    slug: 'fernando',
+    name: 'FerNANDo',
+  },
+
+  // Line 3
+  {
+    type: 'language',
+    slug: 'cardinal',
+    name: 'Cardinal',
+  }, {
+    type: 'language',
+    slug: 'befunge98',
+    name: 'Befunge-98',
+  }, {
+    type: 'language',
+    slug: 'notenglish',
+    name: '~English',
+  }, {
+    type: 'language',
+    slug: 'whitespace',
+    name: 'Whitespace',
+  }, {
+    type: 'language',
+    slug: 'brainfuck',
+    name: 'Brainfuck',
+  }, {
+    type: 'language',
+    slug: 'piet',
+    name: 'Piet',
+  }, {
+    type: 'language',
+    slug: 'emoji',
+    name: 'Emoji',
+  }, {
+    type: 'language',
+    slug: 'snowman',
+    name: 'Snowman',
+  }, {
+    type: 'language',
+    slug: 'jellyfish',
+    name: 'Jellyfish',
+  },
+
+  // Line 4
+  {
+    type: 'language',
+    slug: 'wierd',
+    name: 'Wierd',
+  }, {
+    type: 'language',
+    slug: 'ppap',
+    name: 'PPAP',
+  }, {
+    type: 'language',
+    slug: 'csharp',
+    name: 'C#',
+  }, {
+    type: 'language',
+    slug: 'node',
+    name: 'Node.js',
+  }, {
+    type: 'language',
+    slug: 'ruby2.4',
+    name: 'Ruby 2.4',
+  }, {
+    type: 'language',
+    slug: 'php',
+    name: 'PHP',
+  }, {
+    type: 'language',
+    slug: 'bash-pure',
+    name: 'Bash (builtins)',
+  }, {
+    type: 'language',
+    slug: 'pure-folders',
+    name: 'Pure Folders',
+  }, {
+    type: 'language',
+    slug: 'ruby2',
+    name: 'Word!CPU',
+  },
+
+  // Line 5
+  {
+    type: 'language',
+    slug: 'grass',
+    name: 'Grass',
+  }, {
+    type: 'language',
+    slug: 'emojicode',
+    name: 'Emojicode',
+  }, {
+    type: 'language',
+    slug: 'gs2',
+    name: 'gs2',
+  }, {
+    type: 'base',
+    team: 0,
+  }, {
+    type: 'language',
+    slug: 'perl',
+    name: 'Perl',
+  }, {
+    type: 'base',
+    team: 1,
+  }, {
+    type: 'language',
+    slug: 'golfscript',
+    name: 'GolfScript',
+  }, {
+    type: 'language',
+    slug: 'fugue',
+    name: 'Fugue',
+  }, {
+    type: 'language',
+    slug: 'stuck',
+    name: 'Stuck',
+  },
+
+  // Line 6
+  {
+    type: 'language',
+    slug: 'ffb',
+    name: 'Foobar and Foobaz and Barbaz, oh my!',
+  }, {
+    type: 'language',
+    slug: 'hexagony',
+    name: 'Hexagony',
+  }, {
+    type: 'language',
+    slug: 'streem',
+    name: 'Streem',
+  }, {
+    type: 'language',
+    slug: 'python3',
+    name: 'Python 3',
+  }, {
+    type: 'language',
+    slug: 'c-gcc',
+    name: 'C (GCC)',
+  }, {
+    type: 'language',
+    slug: 'd-gdc',
+    name: 'D (GDC)',
+  }, {
+    type: 'language',
+    slug: 'z80',
+    name: 'Z80',
+  }, {
+    type: 'language',
+    slug: 'logicode',
+    name: 'Logicode',
+  }, {
+    type: 'language',
+    slug: '2sable',
+    name: '2sable',
+  },
+
+  // Line 7
+  {
+    type: 'language',
+    slug: 'axo',
+    name: 'Axo',
+  }, {
+    type: 'language',
+    slug: 'lolcode',
+    name: 'LOLCODE',
+  }, {
+    type: 'language',
+    slug: 'concise-folders',
+    name: 'Concise Folders',
+  }, {
+    type: 'language',
+    slug: 'octave',
+    name: 'Octave',
+  }, {
+    type: 'language',
+    slug: 'cy',
+    name: 'Cy',
+  }, {
+    type: 'language',
+    slug: 'vim',
+    name: 'Vim',
+  }, {
+    type: 'language',
+    slug: 'minus',
+    name: 'Minus',
+  }, {
+    type: 'language',
+    slug: 'blc',
+    name: 'Binary lambda calculus',
+  }, {
+    type: 'language',
+    slug: 'beam',
+    name: 'Beam',
+  },
+
+  // Line 8
+  {
+    type: 'language',
+    slug: 'trumpscript',
+    name: 'TrumpScript',
+  }, {
+    type: 'language',
+    slug: 'stackcats',
+    name: 'Stackcats',
+  }, {
+    type: 'language',
+    slug: 'Unlambda',
+    name: 'unlambda',
+  }, {
+    type: 'language',
+    slug: 'spl',
+    name: 'Shakespeare',
+  }, {
+    type: 'language',
+    slug: '05ab1e',
+    name: '05AB1E',
+  }, {
+    type: 'language',
+    slug: 'rail',
+    name: 'Rail',
+  }, {
+    type: 'language',
+    slug: 'starry',
+    name: 'Starry',
+  }, {
+    type: 'language',
+    slug: 'jelly',
+    name: 'Jelly',
+  }, {
+    type: 'language',
+    slug: 'minimal2d',
+    name: 'Minimal-2D',
+  },
+
+  // Line 9
+  {
+    type: 'language',
+    slug: 'velato',
+    name: 'Velato',
+  }, {
+    type: 'language',
+    slug: 'zombie',
+    name: 'ZOMBIE',
+  }, {
+    type: 'language',
+    slug: 'recurse',
+    name: 'Recurse',
+  }, {
+    type: 'language',
+    slug: 'path',
+    name: 'PATH',
+  }, {
+    type: 'language',
+    slug: 'slashes',
+    name: '///',
+  }, {
+    type: 'language',
+    slug: 'labyrinth',
+    name: 'Labyrinth',
+  }, {
+    type: 'language',
+    slug: 'floater',
+    name: 'Floater',
+  }, {
+    type: 'language',
+    slug: 'haystack',
+    name: 'Haystack',
+  }, {
+    type: 'language',
+    slug: 'malbolge',
+    name: 'Malbolge',
+  },
+];
