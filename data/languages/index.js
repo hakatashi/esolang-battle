8e72804d81ac9d41d580023fb9a30191b290d568
Koki Takahashi <hakatasiloving@gmail.com>
2018-04-29 13:46:36 +0900

    Add language.json

--- data/languages/index.js
+++ data/languages/index.js
@@ -1,9 +1,11 @@
 const languages1 = require('./1.js');
 const languages2 = require('./2.js');
 const languages3 = require('./3.js');
+const languages4 = require('./4.js');
 
 module.exports = {
 	1: languages1,
 	2: languages2,
 	3: languages3,
+	4: languages4,
 };
