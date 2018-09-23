e33841de75c149363c13c35a65cfa4426183fa62
Koki Takahashi <hakatasiloving@gmail.com>
2018-09-23 12:38:56 +0900

    Fix callback ULR

--- config/passport.js
+++ config/passport.js
@@ -33,7 +33,7 @@ passport.use(
 		{
 			consumerKey: process.env.TWITTER_KEY,
 			consumerSecret: process.env.TWITTER_SECRET,
-			callbackURL: 'http://127.0.0.1:3000/auth/twitter/callback',
+			callbackURL: `${process.env.SERVER_ORIGIN}/auth/twitter/callback`,
 			passReqToCallback: true,
 		},
 		// eslint-disable-next-line max-params
