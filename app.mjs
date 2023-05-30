e5af49dbe31c0caea676e49e7d3c52a27b0fc316
fabon <syobon.hinata.public@gmail.com>
2023-05-31 03:37:21 +0900

    Use fs/promises

--- app.mjs
+++ app.mjs
@@ -2,10 +2,9 @@
  * Module dependencies.
  */
 import crypto from 'node:crypto';
-import fs from 'node:fs';
+import fs from 'node:fs/promises';
 import path from 'node:path';
 import {fileURLToPath} from 'node:url';
-import util from 'node:util';
 import chalk from 'chalk';
 import compression from 'compression';
 import MongoStore from 'connect-mongo';
@@ -122,8 +121,7 @@ app.use((req, res, next) => {
 app.use(lusca.xframe('SAMEORIGIN'));
 app.use(lusca.xssProtection(true));
 app.use(async (req, res, next) => {
-	const hash = await util
-		.promisify(fs.readFile)(path.resolve(__dirname, '.git/refs/heads/master'))
+	const hash = await fs.readFile(path.resolve(__dirname, '.git/refs/heads/master'))
 		.catch(() => Math.floor(Math.random() * 1e10));
 
 	res.locals.user = req.user;
